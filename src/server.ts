8ed469fd87217dbbb1ff15f263d8bb3a25b9ee5b
Ilya P <32912855+flowwishthebest@users.noreply.github.com>
2019-10-28 22:39:06 +0300

    Smth (#2)

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,36 @@
-const http = require('http');
+import * as Express from "express";
+import * as BodyParser from "body-parser";
 
-const server = http.createServer();
+class ShortenUrl {
+    public readonly id: number;
+    public readonly sourceUrl: string;
+    public readonly shortUrl: string;
+    public readonly expiration: number;
+}
 
-server.on('request', (req, res) => {
-    res.end('Hello, World!');
+const app = Express();
+
+app.use(BodyParser());
+
+const linksStorage: ShortenUrl[] = [];
+
+app.post("/links", (req, res) => {
+    const link = req.body.source;
+
+    const existingUrl = linksStorage.find((l) => l.sourceUrl === link);
+
+    if (!existingUrl) {
+        res.status(201);
+        res.json({
+            data: {
+                hash: link + Math.random(),
+            },
+        });
+    } else {
+        throw new Error("Url already exists");
+    }
 });
 
-server.listen(3001, () => {
-    console.log('The server running on http://localhost:3001');
+app.listen(3001, () => {
+    console.log("The server running on http://localhost:3001");
 });
