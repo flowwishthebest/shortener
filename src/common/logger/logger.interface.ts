b8dd096c2df57db13bc08c06c7e71dab2ea6c291
Ilya P <32912855+flowwishthebest@users.noreply.github.com>
2019-11-15 14:56:35 +0300

    Added logger (#4)

---
+++ src/common/logger/logger.interface.ts
@@ -0,0 +1,3 @@
+export interface ILogger {
+    info(...args: any[]): void;
+}
--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,35 @@ import * as Express from "express";
 import * as SwaggerUI from "swagger-ui-express";
 import * as SwaggerDocument from "../docs/swagger/v1/swagger.json";
 
-const app = Express();
+import { WinstonLogger } from "./common/logger/winston.logger";
+import { ILogger } from "./common/logger/logger.interface.js";
 
-app.use("/docs/swagger/v1", SwaggerUI.serve, SwaggerUI.setup(SwaggerDocument));
+class Application {
+    private readonly server: Express.Application;
 
-app.get("api/v1/links", (req, res) => {
-    res.status(200);
-    res.write("Hello, World!");
-    res.end();
-});
+    constructor(
+        private readonly logger: ILogger = new WinstonLogger(),
+    ) {
+        this.server = Express();
 
-app.listen(3001, () => {
-    console.log("The server running on http://localhost:3001");
-});
+        this.server.use(
+            "/docs/swagger/v1",
+            SwaggerUI.serve,
+            SwaggerUI.setup(SwaggerDocument),
+        );
+
+        this.server.get("api/v1/links", (req, res) => {
+            res.status(200);
+            res.write("Hello, World!");
+            res.end();
+        });
+    }
+
+    public run(): void {
+        this.server.listen(3001, () => {
+            this.logger.info("The server running on http://localhost:3001");
+        });
+    }
+}
+
+new Application().run();
