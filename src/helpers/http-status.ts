80a4faac2e5dc1481a93f677cb5013eaf12a8cb0
Ilya P <iopolyus@mts.ru>
2021-08-06 13:54:13 +0300

    test(): add test

---
+++ src/helpers/http-status.ts
@@ -0,0 +1,3 @@
+export enum EHttpStatus {
+    OK = 200,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
-import fastify from "fastify";
 import * as config from './config';
+import { build } from './app';
 
-const server = fastify();
-
-server.get("/", async (req, rep) => {
-    return {
-        data: "Hello, World!",
-    };
-});
-
-server.get("/ping", async (req, rep) => {
-    return {
-        data: "Pong!",
-    };
-});
+const server = build({});
 
 server.listen(config.app.port, config.app.host, (err, addr) => {
     if (err) {
